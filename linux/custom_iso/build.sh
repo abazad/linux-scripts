548fce4e945bce9c6469fcddb0ba9c3ce0d944eb
djb77 <dwayne.bakewell@gmail.com>
2016-11-14 17:02:50 +1100

    Updates to Ubuntu ISO Building Script
    - Add I386 Architecture
    - Add extra Repositories
    - Change Java JRE to Java JDK
    
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- linux/custom_iso/build.sh
+++ linux/custom_iso/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Ubuntu Custom DVD Generator Script v1.00 (2016-09-18)
+# Ubuntu Custom DVD Generator Script v1.00 (2016-09-17)
 # -----------------------------------------------------
-# Created by djb77 from various sources, mainly official Ubuntu Documentation
-# Built for and tested on a clean Ubuntu 16.04.1 LTS x64 ISO image
+# Created by djb77 from various sources
+# Tested on a clean Ubuntu 16.04.1 LTS x64 ISO image
 
-# Input ISO File: ~/custom-iso/ubuntu.iso
-# Output ISO File: ~/custom-iso/ubuntu-new.iso
+# Input ISO File: ~/custom-iso/ubuntu-16.04.1-desktop-amd64.iso
+# Output ISO File: ~/custom-iso/ubuntu-16.04.1-desktop-amd64-CUSTOM.iso
 
 # HOW TO USE:
 # Create a new folder called custom-iso in your home directory, and copy the ubuntu-16.04.1-desktop-amd64.iso and this file into that folder.
@@ -18,7 +18,7 @@
 sudo apt-get -y install squashfs-tools genisoimage
 
 mkdir mnt
-sudo mount -o loop ubuntu.iso mnt
+sudo mount -o loop ubuntu-16.04.1-desktop-amd64.iso mnt
 mkdir extract
 sudo rsync --exclude=/casper/filesystem.squashfs -a mnt/ extract
 sudo unsquashfs mnt/casper/filesystem.squashfs
@@ -38,6 +38,14 @@ ln -s /bin/true /sbin/initctl
 
 # MAKE CHANGES / ADD PACKAGES
 #----------------------------
+# Add i386 Architecture
+dpkg --add-architecture i386
+apt-get update
+
+# Add Extra Repositories (Needed to slipstream certain packages into ISO)
+add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+apt-get update
+
 # Remove All Bloat
 # apt-get -y remove aisleriot cheese firefox firefox-locale-en libreoffice-avmedia-backend-gstreamer libreoffice-base-core libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gnome libreoffice-gtk libreoffice-help-en-us libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-style-breeze libreoffice-style-galaxy libreoffice-writer gnome-mahjongg gnome-sudoku gnome-mines thunderbird thunderbird-gnome-support thunderbird-locale-en thunderbird-locale-en-us transmission-common transmission-gtk
 
@@ -72,7 +80,7 @@ ln -s /bin/true /sbin/initctl
 rm -rf /usr/share/applications/ubuntu-amazon-default.desktop
 
 # Install Git
-apt-get -y install git 
+apt-get -y install git
 
 # Install p7zip
 apt-get -y install p7zip-full p7zip-rar
@@ -81,7 +89,10 @@ apt-get -y install p7zip-full p7zip-rar
 apt-get -y install gawk lzop liblz4-tool bison gperf build-essential zlib1g-dev zlib1g-dev:i386 g++-multilib libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev libsepol1-dev dpkg-dev make ccache automake squashfs-tools schedtool
 
 # Install Java
-apt-get -y install default-jre
+apt-get -y install default-jdk
+
+# Install Tweak Tools
+apt-get -y install unity-tweak-tool gnome-tweak-tool
 
 # Install Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
@@ -124,5 +135,6 @@ find -type f -print0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo t
 
 # CREATE ISO IMAGE
 # ----------------
-sudo genisoimage -D -r -V "Ubuntu 16.04.1-djb77_custom" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../ubuntu-new.iso .
+build_date=`date +%Y%m%d`
+sudo genisoimage -D -r -V "Ubuntu 16.04-djb77_$build_date" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../ubuntu-16.04.1-desktop-amd64-CUSTOM.iso .
 
